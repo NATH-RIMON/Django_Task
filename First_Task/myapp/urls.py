d0d22bef722230ff0382aaa4fb9c583913c88221
[200~NATH-RIMON <rimonnath11@gmail.com>
2023-02-12 16:56:43 +0600

    css

--- First_Task/myapp/urls.py
+++ First_Task/myapp/urls.py
@@ -2,9 +2,11 @@
 from django.urls import path
 from .views import home
 from .views import base
+from .views import about
 
 urlpatterns = [
     path('',home),
     path('base',base),
+    path('about',about),
     
 ]
--- First_Task/myapp/views.py
+++ First_Task/myapp/views.py
@@ -5,3 +5,5 @@ def home(request):
     return render(request, 'myapp/home.html')
 def base(request):
     return render(request, 'myapp/base.html')
+def about(request):
+    return render(request,'myapp/about.html')
